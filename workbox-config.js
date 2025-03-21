6a0737a92092ea7f0ec1167dab07de195be34f18
Jonathan Konde <jonathankonde430@gmail.com>
2025-03-21 15:53:09 +0100

    update

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,16 @@
 module.exports = {
     globDirectory: 'build/',
-    globPatterns: ['**/*.{js,css,html,mp4,webm}'],
+    globPatterns: ['**/*.{js,css,html,mp4,webm,png,svg}'],
     swDest: 'build/service-worker.js',
+    maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // Augmentez la limite à 10MB
     runtimeCaching: [{
       urlPattern: /\.(mp4|webm)/,
-      handler: 'CacheFirst'
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'videos',
+        expiration: {
+          maxEntries: 10,
+        },
+      }
     }]
   };
